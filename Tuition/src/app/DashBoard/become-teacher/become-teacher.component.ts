b77ac42cac76d25948a7366696cfda64d3047cc5
YuvalDP <yuvalprajapati572@gmail.com>
2018-03-10 14:09:29 +0530

    dashboard folder added

---
+++ Tuition/src/app/DashBoard/become-teacher/become-teacher.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-become-teacher',
+  templateUrl: './become-teacher.component.html',
+  styleUrls: ['./become-teacher.component.css']
+})
+export class BecomeTeacherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
